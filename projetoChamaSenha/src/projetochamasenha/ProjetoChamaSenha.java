dd9547a8f472eda717f652f014ea2cd3a0f23e88
Pedro Faleiros <142272176+pedrofaleirosss@users.noreply.github.com>
2025-06-30 21:40:55 -0300

    adicionando projeto chama senha

---
+++ projetoChamaSenha/src/projetochamasenha/ProjetoChamaSenha.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetochamasenha;
+
+import telas.chamaSenha;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ProjetoChamaSenha {
+
+
+    public static void main(String[] args) {
+        chamaSenha tela = new chamaSenha();
+        tela.setVisible(true);
+    }
+    
+}
