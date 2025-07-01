8f818703766f89ebc1ec41591cdc293fc664010f
Pedro Faleiros <142272176+pedrofaleirosss@users.noreply.github.com>
2025-06-30 21:41:27 -0300

    adicionando projeto senha

---
+++ projetoSenha/src/projetosenha/ProjetoSenha.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetosenha;
+
+import telas.pegarSenha;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ProjetoSenha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        pegarSenha tela = new pegarSenha();
+        tela.setVisible(true);
+    }
+    
+}
